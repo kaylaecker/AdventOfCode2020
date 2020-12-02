cd3a2b2cc46f48e6349166caa18fe350860e1b40
kaylaecker <kayjaysrainbows@gmail.com>
2020-12-02 00:34:54 -0800

    completed day 1 part 2 and updated time/space for day1 part1

--- src/dayOne/index.js
+++ src/dayOne/index.js
@@ -3,6 +3,8 @@ import filereader from '../utils/filereader.js';
 
 const inputArray = filereader('input.txt').join('').split('\n').map(Number);
 
+/**********      PART 1     **********/
+//time&space is o(n)
 function updateExpenseReport(expensesArray, n = 2020) {
   if(!expensesArray) throw Error('Give me the DATA!')
   let expensesSet = new Set()
@@ -20,3 +22,31 @@ function updateExpenseReport(expensesArray, n = 2020) {
 }
 
 console.log(updateExpenseReport(inputArray, 2020))
+
+
+/**********      PART 2     **********/
+//space o(n), time o(n^2)
+function updateExpenseReportByThree(expensesArray, n = 2020) {
+  if(!expensesArray) throw Error('Give me the DATA!')
+  let expensesSet = new Set()
+
+  for(let i=0;i<expensesArray.length;i++){
+    if(expensesSet.has(expensesArray[i])){
+      if(expensesArray[i] === n/2) return n*n
+    } else {
+      expensesSet.add(expensesArray[i])
+    }
+  }
+
+  for(let i=1; i<expensesArray.length; i++){
+    for(let j=2; j<expensesArray.length; j++){
+      if(expensesSet.has(n-(expensesArray[i] + expensesArray[j]))){
+        console.log(expensesArray[i], expensesArray[j], n-(expensesArray[i] + expensesArray[j]))
+        return expensesArray[i] * expensesArray[j] * (n-(expensesArray[i] + expensesArray[j]))
+      }
+    }
+  }
+  return "no dice"
+}
+
+console.log(updateExpenseReportByThree(inputArray, 2020))
